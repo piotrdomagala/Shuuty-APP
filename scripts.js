e3a519ec845bcebe287e4c750cc0b57a9db95365
piotrdomagala <147268913+piotrdomagala@users.noreply.github.com>
2024-06-18 23:21:14 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,7 +7,6 @@ function applyLanguage(lang) {
         en: {
             currentLanguage: 'English',
             languageBtn: '<img src="images/english_icon.png" alt="English"> English',
-            title: 'Shuuty',
             mainTitle: 'Find Those You Seek',
             description: 'Arrange activities wherever you are.<br>Meet new people similar to you and<br>plan your time like never before.',
             downloadTitle: 'Download the app on mobile',
@@ -19,7 +18,6 @@ function applyLanguage(lang) {
         pl: {
             currentLanguage: 'Polski',
             languageBtn: '<img src="images/polish_icon.png" alt="Polski"> Polski',
-            title: 'Shuuty',
             mainTitle: 'Znajdź tych których szukasz',
             description: 'Umów się na aktywność, gdziekolwiek jesteś.<br>Poznaj nowych ludzi podobnych do Ciebie oraz<br>zaplanuj swój czas jak nigdy wcześniej.',
             downloadTitle: 'Pobierz aplikację na urządzenia mobilne',
@@ -35,7 +33,6 @@ function applyLanguage(lang) {
     const elements = {
         currentLanguage: document.getElementById('current-language'),
         languageBtn: document.getElementById('language-btn'),
-        title: document.getElementById('title'),
         mainTitle: document.getElementById('main-title'),
         description: document.getElementById('description'),
         downloadTitle: document.getElementById('download-title'),
@@ -70,25 +67,28 @@ document.addEventListener('DOMContentLoaded', function() {
     // Set default language to English
     applyLanguage('en');
 
-    // Infinite scroll functionality
-    const leftSection = document.querySelector('.left-section');
-    const images = Array.from(leftSection.querySelectorAll('img'));
+    // Infinite scroll functionality for mobile only
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        const leftSection = document.querySelector('.left-section');
+        const images = Array.from(leftSection.querySelectorAll('img'));
 
-    // Clone the images for infinite scroll effect
-    images.forEach(img => {
-        const clone = img.cloneNode(true);
-        leftSection.appendChild(clone);
-    });
+        // Clone the images for infinite scroll effect
+        images.forEach(img => {
+            const clone = img.cloneNode(true);
+            leftSection.appendChild(clone);
+        });
 
-    let isScrolling;
-    leftSection.addEventListener('scroll', () => {
-        window.clearTimeout(isScrolling);
+        let isScrolling;
+        leftSection.addEventListener('scroll', () => {
+            window.clearTimeout(isScrolling);
 
-        isScrolling = setTimeout(() => {
-            if (leftSection.scrollLeft >= leftSection.scrollWidth / 2) {
-                leftSection.scrollLeft -= leftSection.scrollWidth / 2;
-            }
-        }, 100);
-    });
+            isScrolling = setTimeout(() => {
+                if (leftSection.scrollLeft >= leftSection.scrollWidth / 2) {
+                    leftSection.scrollLeft -= leftSection.scrollWidth / 2;
+                } else if (leftSection.scrollLeft <= 0) {
+                    leftSection.scrollLeft += leftSection.scrollWidth / 2;
+                }
+            }, 100);
+        });
+    }
 });
-
