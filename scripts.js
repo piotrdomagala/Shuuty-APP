5cdc51f4ad91ae8db4f265d18128404deec8f11d
piotrdomagala <147268913+piotrdomagala@users.noreply.github.com>
2024-07-02 12:17:53 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,14 +3,12 @@ document.addEventListener('DOMContentLoaded', function() {
         item.addEventListener('click', event => {
             event.preventDefault();
             const lang = event.currentTarget.getAttribute('data-lang');
-            setCookie('lang', lang, 365);
-            applyLanguage(lang);
+            changeLanguage(lang);
         });
     });
 
-    // Set default language to English or get from cookies
-    const defaultLang = getCookie('lang') || 'en';
-    applyLanguage(defaultLang);
+    // Set default language to English
+    applyLanguage('en');
 
     // Event listeners for the links in the footer
     document.getElementById('privacy-link').addEventListener('click', function(event) {
@@ -26,7 +24,16 @@ document.addEventListener('DOMContentLoaded', function() {
         const file = lang === 'pl' ? 'documents/terms.html' : 'documents/terms_en.html';
         window.location.href = file;
     });
+
+    // Monitor orientation changes
+    window.addEventListener('orientationchange', handleOrientationChange);
 });
+
+function handleOrientationChange() {
+    // Reset scroll position to top when orientation changes
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
 function setCookie(name, value, days) {
     const d = new Date();
@@ -49,6 +56,11 @@ function getCookie(name) {
         }
     }
     return "";
+}
+
+function changeLanguage(lang) {
+    setCookie('lang', lang, 365);
+    applyLanguage(lang);
 }
 
 function applyLanguage(lang) {
